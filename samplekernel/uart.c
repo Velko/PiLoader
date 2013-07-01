5f0d58f841f62a79f0b3c7b5fa0cca9545bb8c53
Jurģis Brigmanis <velko.x@gmail.com>
2013-07-01 23:06:21 +0300

    Sample kernel ported to UART0.

--- samplekernel/uart.c
+++ samplekernel/uart.c
@@ -25,18 +25,10 @@
 void uart_init()
 {
     volatile unsigned i;
-    AUX_ENABLES = 1;    // AUX enable
-    AUX_MU_IER_REG = 0; // interrupt enable = 0
-    AUX_MU_CNTL_REG = 0;// extra control
-    AUX_MU_LCR_REG = 3; // line control
-    AUX_MU_MCR_REG = 0; // modem control
-    AUX_MU_IER_REG = 0; // interrupt enable = 0 (again???)
-    AUX_MU_IIR_REG = 0xC6; // interrupt identify
-    AUX_MU_BAUD_REG = 270; // speed
 
+    UART0_CR = 0;
 
-    GPFSEL1 = (GPFSEL1 & ~(7<<FSEL14OFF)) | (2<<FSEL14OFF);
-    GPFSEL1 = (GPFSEL1 & ~(7<<FSEL15OFF)) | (2<<FSEL15OFF);
+    GPFSEL1 = (GPFSEL1 & ~(7<<FSEL14OFF)) | (4<<FSEL14OFF);
 
     GPPUD = 0;
     for (i=0; i < 150; i++);
@@ -44,20 +36,24 @@ void uart_init()
     for (i=0; i < 150; i++);
     GPPUDCLK0 = 0;
 
-    AUX_MU_CNTL_REG = 3;
+    UART0_ICR = 0x7FF;
+    UART0_IBRD = 1;
+    UART0_FBRD = 40;
+    UART0_LCRH = 0x70;
+    UART0_CR = 0x301;
 }
 
 void uart_putc(char c)
 {
-    /* Wait until the serial buffer is ready */
-    while ((AUX_MU_LSR_REG & (1 << 5)) == 0);
+    /* Wait until there's some space in FIFO */
+    while (UART0_FR & UART0_FR_TXFF);
 
-    /* Put character into the serial buffer */
-    AUX_MU_IO_REG = c;
+    /* Put character there */
+    UART0_DR = c;
 }
 
 void uart_waitidle()
 {
-    /* Wait until the serial has finished to send. */
-    while ((AUX_MU_LSR_REG & (1 << 6)) == 0);
+    /* Wait until the FIFO is clear and UART has finished with last byte. */
+    while ( (UART0_FR & UART0_FR_TXFE ) == 0 || (UART0_FR & UART0_FR_BUSY));
 }
