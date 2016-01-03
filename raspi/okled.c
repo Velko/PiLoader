f8c3d8e8f1b6b57fefd9f1e0d611133afc48401d
Jurģis Brigmanis <velko.x@gmail.com>
2016-01-03 15:31:48 +0200

    Fixed OK led for Raspberry Pi 2
    Fixes #8

--- raspi/okled.c
+++ raspi/okled.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013 Jurģis Brigmanis
+/* Copyright (c) 2013-2016 Jurģis Brigmanis
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,9 +22,11 @@
 #include "raspihw.h"
 #include "bcm283x.h"
 
+#ifdef TARGET_BOARD_RPI
+
 void okled_init()
 {
-    /* OK LED is connected to pin GPIO16.
+    /* Original Raspberry Pi's OK LED is connected to pin GPIO16.
 
        Will configure it as an output (set FSEL16
        bits to 001 - output).
@@ -41,3 +43,29 @@ void okled_switch(bool on)
         GPFSET0 = 1<<GPSET16;
     }
 }
+
+#endif
+
+#ifdef TARGET_BOARD_RPI2
+
+void okled_init()
+{
+    /* Raspberry Pi 2's OK LED is connected to pin GPIO47.
+
+       Will configure it as an output (set FSEL47
+       bits to 001 - output).
+     */
+    GPFSEL4 = (GPFSEL4 & ~(7<<FSEL47OFF)) | 1<<FSEL47OFF;
+    GPFSET0 = 1<<GPSET47;
+}
+
+void okled_switch(bool on)
+{
+    if (on) {
+        GPFSET1 = 1<<GPSET47;
+    } else {
+        GPFCLR1 = 1<<GPCLR47;
+    }
+}
+
+#endif
